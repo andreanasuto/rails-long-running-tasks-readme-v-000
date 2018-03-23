4ee9e265d7ccffae0684db7add84340f3918bc2b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-23 19:47:00 +0000

    automatically backed up by learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,6 +9,7 @@ require 'csv'
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       Customer.create(email: leads[0], first_name: leads[1], last_name: leads[2] )
     end
+  redirect_to customers_path
   end
 
 end
